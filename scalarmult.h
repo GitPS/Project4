d45fc44a515a87eca4f1481952052241b4fd6248
GitPS <sime.phil@uwlax.edu>
2013-11-24 15:59:44 -0600

    Upload test results.

--- scalarmult.h
+++ scalarmult.h
@@ -9,17 +9,20 @@
 /*****************************
  * Global Variables
  *****************************/
-int MAX_ITERS = 50;
-
-
-
+int MAX_ITERS = 10000;
 
 /*
- * TODO
+ * Executes a scalar multiplication using a the ij pattern for loop execution.
+ *   matrix : Matrix to be used for calculations  
+ *   scalar : Scalar to use for calculations 
+ *   N : Size of matrix 
  */
 double run_experiment_ij(mtype_t *matrix, mtype_t scalar, int N);
 
 /*
- * TODO
+ * Executes a scalar multiplication using a the ji pattern for loop execution.
+ *   matrix : Matrix to be used for calculations  
+ *   scalar : Scalar to use for calculations 
+ *   N : Size of matrix 
  */
 double run_experiment_ji(mtype_t *matrix, mtype_t scalar, int N);
