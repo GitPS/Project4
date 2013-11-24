2d914940b39074e8555ea59fe4e5a19d221de659
GitPS <sime.phil@uwlax.edu>
2013-11-24 14:36:35 -0600

    Change iterations to actual test value.  Add comments to header files.

--- matrixmult.h
+++ matrixmult.h
@@ -6,34 +6,58 @@
  */
 #include "support.h"
 
-int MAX_ITERS = 1;
+int MAX_ITERS = 100;
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the ijk pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_ijk(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the ikj pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_ikj(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the jki pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_jki(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the jik pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_jik(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the kij pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_kij(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
 
 /*
- * TODO
+ * Executes a matrix multiplication using a the kji pattern for loop execution.
+ *   matrix_a : Matrix A 
+ *   matrix_b : Matrix B
+ *   matrix_c : Matrix C
+ *   N : Size of matrices
  */
 double run_experiment_kji(mtype_t *matrix_a, mtype_t *matrix_b, mtype_t *matrix_c, int N);
